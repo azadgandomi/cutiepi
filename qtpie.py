382a38f18c79c6009c04a7ab99404bd56ed0a1f1
Azad Gandomi <azad.gandomi@gmail.com>
2019-08-27 23:20:40 +0200

    Now we have a persistant camera thread!

--- qtpie.py
+++ qtpie.py
@@ -3,7 +3,7 @@ import threading
 import os
 import sys
 import signal
-import camera_server
+import picamera
 from time import sleep
 from gpiozero import RGBLED
 from gpiozero import DistanceSensor
@@ -13,7 +13,7 @@ signal.signal(signal.SIGINT, signal.default_int_handler)
 
 os.chdir(os.path.dirname(__file__))
 
-PORT_CAMERA = 8082
+PORT_CAMERA = 45678
 PORT_CONTROL = 56789
 SPEED = 0.5
 
@@ -24,10 +24,35 @@ red = (1, 0, 0)
 class CameraThread(threading.Thread):
     def __init__(self):
         super().__init__()
-        self.event = threading.Event()
+        self.isAlive = True
         
     def run(self):
-        camera_server.start(self.event)
+        with picamera.PiCamera(resolution=(480,360), framerate=24) as camera:
+            while self.isAlive:
+                try:
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                        s.bind(('', PORT_CAMERA))
+                        print('Listening for Camera...')
+                        s.listen()
+                        connection, addr = s.accept()
+                        with connection.makefile('wb') as output:
+                            print('Connected for camera by', addr)
+                            camera.start_recording(output, format='h264', profile='main')
+                            while self.isAlive:
+                                camera.wait_recording(0.1)
+                            
+                except:
+                    print("Unexpected camera error type: ", sys.exc_info()[0])
+                    print("Unexpected camera error value: ", sys.exc_info()[1])
+                    
+                try:
+                    camera.stop_recording()
+                except:
+                    print("Exception when stopping recording: ", sys.exc_info()[1])
+                sleep(1)
+        
+    def stop(self):
+        self.isAlive = False
             
 
 
@@ -63,12 +88,12 @@ class DistanceThread(threading.Thread):
     def __init__(self):
         super().__init__()
         self.isObstacleClose = False
-        self.stopped = False
+        self.isAlive = True
         self.color = (0, 0, 0)
         
     def run(self):
         with DistanceSensor(26,19, max_distance=4.5) as sensor:
-            while(not self.stopped):
+            while self.isAlive:
                 if(sensor.distance < 0.25):
                     if not self.isObstacleClose:
                         stop()
@@ -79,13 +104,15 @@ class DistanceThread(threading.Thread):
                     led.color = self.color
                     
                 sleep(0.05)
-            
+                
+    def stop(self):
+        self.isAlive = False
+        
             
 isOn = True
 with RGBLED(22, 27, 10, False) as led,\
      Motor(forward=2, backward=3) as motorL,\
      Motor(forward=4, backward=17) as motorR:
-
     distanceSystem = DistanceThread()
     distanceSystem.start()
     cameraSystem = CameraThread()
@@ -100,7 +127,7 @@ with RGBLED(22, 27, 10, False) as led,\
                 b'RA' : rotateAntiClockwise}
          
     while isOn:
-        try:
+        try:    
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.bind(('', PORT_CONTROL))
                 print('Listening for control...')
@@ -127,16 +154,15 @@ with RGBLED(22, 27, 10, False) as led,\
                             continue
                         else:
                             commands[data]()
-        except OSError as e:
-            print("Unexpected error: ", e)
         except:
-            print("Unexpected error: ", sys.exc_info()[0])
-            break;
+            print("Unexpected unkown error: ", sys.exc_info()[0])
+            isOn = False
         sleep(1)
-    distanceSystem.stopped = True
-    cameraSystem.event.set()
-    distanceSystem.join()
+        
+    cameraSystem.stop()
     cameraSystem.join()
+    distanceSystem.stop()
+    distanceSystem.join()
     stop()
     led.off()
 print('Done')
