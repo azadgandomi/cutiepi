61c358ebbdf99edab5b372dfa366c066d757aec1
Azad Gandomi <azad.gandomi@gmail.com>
2019-09-30 22:02:35 +0200

    Small fixes to circuit diagram and code.

--- cutiepi.py
+++ cutiepi.py
@@ -7,7 +7,6 @@ import picamera
 from time import sleep
 from gpiozero import RGBLED
 from gpiozero import DistanceSensor
-from gpiozero import DigitalOutputDevice
 from gpiozero import Motor
 
 signal.signal(signal.SIGINT, signal.default_int_handler)
@@ -25,16 +24,14 @@ LED_GREEN = (0, 1, 0)
 LED_RED = (1, 0, 0)
 
 
-
 class CameraThread(threading.Thread):
     def __init__(self, ready):
         super().__init__()
         self.ready = ready
         self.isAlive = True
-        
-        
+
     def run(self):
-        with picamera.PiCamera(resolution=(800,400), framerate=24) as camera:
+        with picamera.PiCamera(resolution=(800, 400), framerate=24) as camera:
             while self.isAlive:
                 try:
                     self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,8 +46,7 @@ class CameraThread(threading.Thread):
                             print('Connected for camera by', addr)
                             camera.start_recording(output, format='mjpeg')
                             while self.isAlive:
-                                camera.wait_recording(0.1)                            
-                    
+                                camera.wait_recording(0.1)
                 except:
                     print("camera error type: ", sys.exc_info()[0])
                     print("camera error value: ", sys.exc_info()[1])
@@ -59,8 +55,7 @@ class CameraThread(threading.Thread):
                     camera.stop_recording()
                 except:
                     pass
-                
-        
+
     def stop(self):
         self.isAlive = False
         try:
@@ -72,26 +67,32 @@ class CameraThread(threading.Thread):
 def stop():
     motorL.stop()
     motorR.stop()
-    
+
+
 def goForward():
     motorL.forward(power)
     motorR.forward(power)
-        
+
+
 def goBackward():
     motorL.backward(power)
     motorR.backward(power)
-    
+
+
 def turnRight():
     motorL.forward(power)
     motorR.stop()
-    
+
+
 def turnLeft():
     motorL.stop()
     motorR.forward(power)
-    
+
+
 def rotateClockwise():
     motorL.forward(power)
     motorR.backward(power)
+
 
 def rotateAntiClockwise():
     motorL.backward(power)
@@ -100,36 +101,36 @@ def rotateAntiClockwise():
 
 isObstacleClose = False
 
+
 def obstacleInRange():
     global isObstacleClose
     isObstacleClose = True
     stop()
     led.color = LED_RED
 
+
 def obstacleOutOfRange():
     global isObstacleClose
     isObstacleClose = False
     led.color = LED_GREEN
-
         
             
 isOn = True
 
-with RGBLED(22, 27, 10, False) as led,\
-     DigitalOutputDevice(pin=14, initial_value=True),\
-     Motor(forward=2, backward=3) as motorL,\
-     Motor(forward=4, backward=17) as motorR,\
+with RGBLED(22, 27, 17, False) as led,\
+     Motor(forward=14, backward=15) as motorL,\
+     Motor(forward=3, backward=2) as motorR,\
      DistanceSensor(26,19, threshold_distance=0.2, max_distance=4.5) as distanceSensor:
 
-    restrictedCommands = {b'ST' : stop,
-                          b'GB' : goBackward,
-                          b'RC' : rotateClockwise,
-                          b'RA' : rotateAntiClockwise}
+    restrictedCommands = {b'ST': stop,
+                          b'GB': goBackward,
+                          b'RC': rotateClockwise,
+                          b'RA': rotateAntiClockwise}
 
     commands = {**restrictedCommands,
-                b'GF' : goForward,
-                b'TR' : turnRight,
-                b'TL' : turnLeft}
+                b'GF': goForward,
+                b'TR': turnRight,
+                b'TL': turnLeft}
 
     while isOn:
         try:    
@@ -154,7 +155,7 @@ with RGBLED(22, 27, 10, False) as led,\
                     while True:
                         data = conn.recv(2)
                         print(data)
-                        if (not data):
+                        if not data:
                             print("socket closed!")
                             break
                         elif data == b'SD':
